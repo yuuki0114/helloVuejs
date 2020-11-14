10a83aec0c086fa0d67f5f09414c1b64f0efb7ad
yuuki0114 <y.no0114@gmail.com>
2020-11-14 09:30:28 +0900

    first commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,33 @@
-new Vue({
+Vue.component('hello-component', {
+  template:'<p>Hello<p>',
+})
+
+var app = new Vue({
+  // option
   el: '#app',
   data: {
-    message: 'Hello Vue.js'
+
+    colors: [
+      'red', 'green', 'blue'
+    ],
+
+    user: {
+      firstName: 'Taro',
+      lastName: ' Yamada',
+      age: 28
+    },
+
+    now: '',
+
+    message: 'Hello Vue.js!',
+
+
+  },
+
+  methods: {
+    onclick: function() {
+      // alert('onclick!');
+      this.now = new Date(). toLocaleString();
+    }
   }
 })
